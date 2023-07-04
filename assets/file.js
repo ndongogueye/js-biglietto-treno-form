e761ad50552e15d5ce19924714f58be6acad7829
ndongogueye <bayeg2003@gmail.com>
2023-07-04 14:49:33 +0200

    biglietto completo

--- assets/file.js
+++ assets/file.js
@@ -1,19 +1,21 @@
+const userNameInput = document.querySelector("[name=user]");
+const kmInput = document.querySelector("[name=km]");
+const etaInput = document.querySelector("[name=eta]");
+const btnGenera = document.querySelector(".genera");
+const btnAnnulla = document.querySelector(".annulla");
 
-
-const prezzoPerKm = 0.21;
-const prezzoBiglietto = kmDaFare * prezzoPerKm;
-
- let sconto = 0;
- if (etaUtente <18 ) {
-    sconto = 20;
- }
-
- else if (etaUtente >= 65) {
-    sconto = 40;
- }
-
- let scontoTotale = prezzoBiglietto * sconto / 100;
-
- const prezzoBigliettoScontato = prezzoBiglietto - scontoTotale;
-
-console.log (prezzoBigliettoScontato)
+btnGenera.addEventListener("click", function () {
+console.log("click")
+    const username = userNameInput.value;
+    const kilometres = kmInput.value;
+    const age = etaInput.value;
+    const pagamento = parseInt(kilometres) * 0.21;
+    if (age < 18) {
+        document.getElementById("costo").innerHTML = (pagamento - (pagamento * 20) / 100 ).toFixed(2);
+    } else if ( age>= 65) {
+        document.getElementById("costo").innerHTML = (pagamento - (pagamento * 20) / 100 ).toFixed(2);
+    } else {
+        document.getElementById("costo").innerHTML = pagamento.toFixed(2);
+    }
+    document.getElementById("name").innerHTML = user;
+})
